caa1a146d42a30cba7e49bf8b38f426b43a6c4a2
Ishwar-iitrpr29 <ishwar.iitrpr@gmail.com>
2025-06-19 18:57:38 +0530

    start deploy

--- Backend/app.js
+++ Backend/app.js
@@ -17,7 +17,7 @@ const reportRouter = require("./routes/Reports");
 
 const app = express()
 
-const port = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cors());
